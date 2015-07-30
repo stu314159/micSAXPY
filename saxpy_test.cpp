e61ba16e5b16d9194cfa2765edcf8949d29f3ad7
Stu Blair <stu314159@gmail.com>
2015-07-30 17:42:16 +0000

    check for command line arguments and exit with error if missing

--- saxpy_test.cpp
+++ saxpy_test.cpp
@@ -1,10 +1,17 @@
-#include <omp.h>
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 int main(int argc, char * argv[]){
   cout << "commencing test of mic..." << endl;
+
+  // verify 2 command line arguments besides the program
+  if(argc < 3) {
+    cerr << "Usage: aprun -cc none -k ./saxpy_test [vec length] [num iterations]" << endl;
+    exit(1);
+  }
+  
 
 return 0;
 }
